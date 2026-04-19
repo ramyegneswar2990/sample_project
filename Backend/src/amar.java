d550f314c38096bf82dd71b3c3f2f71440bf2cca
ramyegneswar2990 <ramyegneswarseeramm@gmail.com>
2026-04-19 18:12:22 +0530

    latest  amar file changes

---
+++ Backend/src/amar.java
@@ -0,0 +1,8 @@
+public class amar {
+    
+    public static  void main(String[] args) {
+        System.out.println("This is the amar class.");
+        System.out.println("We will be adding more functionality to this class in the future.");
+        System.out.println("Stay tuned for updates!");
+    }
+}
