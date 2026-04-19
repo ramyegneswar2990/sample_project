28e7010f6631b766894a5ddebf75cb0c465b4136
sathvik044 <sathvikvadapalli123@gmail.com>
2026-04-19 18:14:45 +0530

    amar vhanges

--- Backend/src/amar.java
+++ Backend/src/amar.java
@@ -4,5 +4,6 @@ public class amar {
         System.out.println("This is the amar class.");
         System.out.println("We will be adding more functionality to this class in the future.");
         System.out.println("Stay tuned for updates!");
+        System.out.println("This class will be used for various purposes in the project.");
     }
 }
