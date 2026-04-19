0faa4d6bce2de87c976b7454ab1ae3a7f3de1582
ramyegneswar2990 <ramyegneswarseeramm@gmail.com>
2026-04-19 18:56:09 +0530

    era changes

--- Backend/src/amar.java
+++ Backend/src/amar.java
@@ -4,5 +4,7 @@ public class amar {
         System.out.println("This is the amar class.");
         System.out.println("We will be adding more functionality to this class in the future.");
         System.out.println("Stay tuned for updates!");
+        int era=20;
+        
     }
 }
