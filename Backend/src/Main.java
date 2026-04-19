2854cf742a4feed1ff826ae16110df3198232607
sathvik044 <sathvikvadapalli123@gmail.com>
2026-04-19 17:59:21 +0530

    extra changes

--- Backend/src/Main.java
+++ Backend/src/Main.java
@@ -2,5 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is the backend of the project.");
+        System.out.println("We will be using Java for the backend development.");
+        System.out.println("Let's get started!");
     }
 }
